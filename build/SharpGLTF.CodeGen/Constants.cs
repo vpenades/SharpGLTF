ec9654a3da49c06e1a15b9ba78562cd1de853239
Vicente Penades Armengot <vpenades@neuroathome.com>
2019-03-06 09:46:33 +0100

    renamed project directory.

--- build/SharpGLTF.CodeGen/Constants.cs
+++ build/SharpGLTF.CodeGen/Constants.cs
@@ -33,7 +33,7 @@ namespace SharpGLTF
         /// <summary>
         /// directory within the solution where the generated code is emitted
         /// </summary>
-        public static string TargetProjectDirectory => "src\\SharpGLTF.DOM\\Schema2\\Generated";
+        public static string TargetProjectDirectory => "src\\SharpGLTF\\Schema2\\Generated";
 
         /// <summary>
         /// namespace of the emitted generated code
