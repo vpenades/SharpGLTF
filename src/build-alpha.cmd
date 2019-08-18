3c18ea2f730101a0ef7d267d46137e97e53175ad
Vicente Penades <megaz0ne23@hotmail.com>
2019-08-19 00:15:57 +0200

    version bump

---
+++ src/build-alpha.cmd
@@ -0,0 +1,7 @@
+@echo off
+set VERSIONSUFFIX=alpha0012
+
+echo Building %VERSIONSUFFIX%
+
+dotnet build -c:Release --version-suffix %VERSIONSUFFIX% SharpGLTF.Core\SharpGLTF.Core.csproj
+dotnet build -c:Release --version-suffix %VERSIONSUFFIX% SharpGLTF.Toolkit\SharpGLTF.Toolkit.csproj
