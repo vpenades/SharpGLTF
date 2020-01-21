07f7b4cf294fefb257c696a8f5641036f049411c
Vicente Penades <megaz0ne23@hotmail.com>
2020-01-21 09:18:34 +0100

    Added convertible interface

---
+++ src/SharpGLTF.Core/Schema2/IConvertibleToGltf2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpGLTF.Schema2
+{
+    /// <summary>
+    /// Defines a method that converts the implementing reference to a <see cref="ModelRoot"/>
+    /// </summary>
+    public interface IConvertibleToGltf2
+    {
+        ModelRoot ToGltf2();
+    }
+}
