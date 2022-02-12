7376d7187593b6766ccf78624cfaf5406a28c93e
Aleksandar Cebov <aleksandar.cebov@3shape.com>
2022-02-12 13:12:59 +0100

    fixing unittests

--- src/SharpGLTF.Toolkit/Geometry/VertexTypes/VertexEmpty.cs
+++ src/SharpGLTF.Toolkit/Geometry/VertexTypes/VertexEmpty.cs
@@ -58,9 +58,9 @@ namespace SharpGLTF.Geometry.VertexTypes
 
         void IVertexMaterial.SetTexCoord(int index, Vector2 coord) { throw new ArgumentOutOfRangeException(nameof(index)); }
 
-        VertexMaterialDelta IVertexMaterial.Subtract(IVertexMaterial baseValue) { throw new NotSupportedException(); }
+        VertexMaterialDelta IVertexMaterial.Subtract(IVertexMaterial baseValue) { return VertexMaterialDelta.Zero; }
 
-        void IVertexMaterial.Add(in VertexMaterialDelta delta) { throw new NotSupportedException(); }
+        void IVertexMaterial.Add(in VertexMaterialDelta delta) { /* do nothing */ }
 
         Vector4 IVertexMaterial.GetColor(int index) { throw new ArgumentOutOfRangeException(nameof(index)); }
 
