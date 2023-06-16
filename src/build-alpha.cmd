afffdb48f8362fee06e0fce67257f33248c1934d
vpenades <5433822+vpenades@users.noreply.github.com>
2023-06-16 17:37:11 +0200

    Added SharpGLTF.Runtime library

--- src/build-alpha.cmd
+++ src/build-alpha.cmd
@@ -4,6 +4,7 @@ set VERSIONSUFFIX=alpha0030
 echo Building %VERSIONSUFFIX%
 
 dotnet build -c:Release --version-suffix %VERSIONSUFFIX% SharpGLTF.Core\SharpGLTF.Core.csproj
+dotnet build -c:Release --version-suffix %VERSIONSUFFIX% SharpGLTF.Runtime\SharpGLTF.Runtime.csproj
 dotnet build -c:Release --version-suffix %VERSIONSUFFIX% SharpGLTF.Toolkit\SharpGLTF.Toolkit.csproj
 
 pause
