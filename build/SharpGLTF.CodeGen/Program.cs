f16732d15c913f6e0c982a6f09d0da3a703812aa
Bert Temme <btemme@hotmail.com>
2023-11-27 13:49:32 +0100

    undo commenting

--- build/SharpGLTF.CodeGen/Program.cs
+++ build/SharpGLTF.CodeGen/Program.cs
@@ -23,42 +23,42 @@ namespace SharpGLTF
 
             processors.Add(new MainSchemaProcessor());
 
-            //// ---------------------------------------------- Add extensions            
+            // ---------------------------------------------- Add extensions            
 
-            //// material extensions       
-            //processors.Add(new UnlitExtension());
-            //processors.Add(new IorExtension());
-            //processors.Add(new SheenExtension());
-            //processors.Add(new VolumeExtension());
-            //processors.Add(new SpecularExtension());
-            //processors.Add(new ClearCoatExtension());
-            //processors.Add(new IridescenceExtension());
-            //processors.Add(new TransmissionExtension());
-            //processors.Add(new EmissiveStrengthExtension());
-            //processors.Add(new SpecularGlossinessExtension());
+            // material extensions       
+            processors.Add(new UnlitExtension());
+            processors.Add(new IorExtension());
+            processors.Add(new SheenExtension());
+            processors.Add(new VolumeExtension());
+            processors.Add(new SpecularExtension());
+            processors.Add(new ClearCoatExtension());
+            processors.Add(new IridescenceExtension());
+            processors.Add(new TransmissionExtension());
+            processors.Add(new EmissiveStrengthExtension());
+            processors.Add(new SpecularGlossinessExtension());
 
-            //// cesium outlines
-            //processors.Add(new CesiumPrimitiveOutlineExtension());
+            // cesium outlines
+            processors.Add(new CesiumPrimitiveOutlineExtension());
 
-            //// lights
-            //processors.Add(new LightsPunctualExtension());
+            // lights
+            processors.Add(new LightsPunctualExtension());
 
-            //// gpu mesh instancing
-            //processors.Add(new MeshGpuInstancingExtension());
+            // gpu mesh instancing
+            processors.Add(new MeshGpuInstancingExtension());
 
-            //// textures
-            //processors.Add(new TextureTransformExtension());
-            //processors.Add(new TextureDDSExtension());
-            //processors.Add(new TextureWebpExtension());
-            //processors.Add(new TextureKtx2Extension());
+            // textures
+            processors.Add(new TextureTransformExtension());
+            processors.Add(new TextureDDSExtension());
+            processors.Add(new TextureWebpExtension());
+            processors.Add(new TextureKtx2Extension());
 
-            //processors.Add(new AgiArticulationsExtension());
-            //processors.Add(new AgiStkMetadataExtension());
+            processors.Add(new AgiArticulationsExtension());
+            processors.Add(new AgiStkMetadataExtension());
 
-            //// other
-            //processors.Add(new XmpJsonLdExtension());
+            // other
+            processors.Add(new XmpJsonLdExtension());
 
-            //processors.Add(new ExtMeshFeaturesExtension());
+            processors.Add(new ExtMeshFeaturesExtension());
 
             processors.Add(new ExtInstanceFeaturesExtension());
 
