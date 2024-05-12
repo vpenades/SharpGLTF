0942c51366f0a81b9a614e1a9e848e70a65be874
vpenades <5433822+vpenades@users.noreply.github.com>
2024-05-12 17:14:12 +0200

    actions++

---
+++ build.sh
@@ -0,0 +1,22 @@
+
+# set input or default
+
+DEFAULTSEMVER="1.0.0-Test-DATE-TIME"
+NUGETSEMVER=${1:-$DEFAULTSEMVER}
+
+# replace date
+DATE_SHORT=$(date +'%Y%m%d')
+NUGETSEMVER="${NUGETSEMVER/DATE/$DATE_SHORT}"
+
+# replace time
+TIME_SHORT=$(date +'%H%M%S')
+NUGETSEMVER="${NUGETSEMVER/TIME/$TIME_SHORT}"
+
+# report semver
+echo "Semver: $NUGETSEMVER";
+
+# build
+
+dotnet restore
+
+dotnet pack -c Release --output "." -p:PackageVersion=$NUGETSEMVER
