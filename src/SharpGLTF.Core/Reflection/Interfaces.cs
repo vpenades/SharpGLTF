42913180e8dd2a5d87debaa0e2b01247c785e6f9
vpenades <5433822+vpenades@users.noreply.github.com>
2025-04-05 14:16:45 +0200

    docs++

--- src/SharpGLTF.Core/Reflection/Interfaces.cs
+++ src/SharpGLTF.Core/Reflection/Interfaces.cs
@@ -6,12 +6,24 @@ using FIELDINFO = SharpGLTF.Reflection.FieldInfo;
 
 namespace SharpGLTF.Reflection
 {
+
+    /// <summary>
+    /// Signals that an object contains a named collection of <see cref="FIELDINFO"/>
+    /// </summary>
+    /// <remarks>
+    /// if an object implementing this interface also implements <see cref="IReflectionArray"/>,
+    /// then <see cref="TryGetField(string, out FIELDINFO)"/> should work using the index
+    /// converted to a string as in <code>TryGetField("17", out var field); field == IReflectionArray.GetField(17);</code>
+    /// </remarks>
     public interface IReflectionObject
     {
         IEnumerable<FIELDINFO> GetFields();
         bool TryGetField(string name, out FIELDINFO value);
     }
 
+    /// <summary>
+    /// Signals that an object contains a variable collection of <see cref="FIELDINFO"/>
+    /// </summary>    
     public interface IReflectionArray : IReflectionObject
     {
         int Count { get; }
