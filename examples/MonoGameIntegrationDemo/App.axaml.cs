b510c150b8b9b3b2cf749072fdd92f50752ec6fc
vpenades <5433822+vpenades@users.noreply.github.com>
2026-04-16 23:49:29 +0200

    fixed example

--- examples/MonoGameIntegrationDemo/App.axaml.cs
+++ examples/MonoGameIntegrationDemo/App.axaml.cs
@@ -20,9 +20,6 @@ namespace MonoGameIntegrationDemo
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
-                // Line below is needed to remove Avalonia data validation.
-                // Without this line you will get duplicate validations from both Avalonia and CT
-                BindingPlugins.DataValidators.RemoveAt(0);
                 desktop.MainWindow = new MainWindow
                 {
                     DataContext = new MainWindowViewModel(),
